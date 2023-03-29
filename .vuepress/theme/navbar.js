cb66d50857f57743a932de5bf971b33d3ab2da4c
Toby <1289691918@qq.com>
2023-03-29 13:46:32 +0800

    Fix: 修改路径

--- .vuepress/theme/navbar.js
+++ .vuepress/theme/navbar.js
@@ -1,5 +1,5 @@
 export default [
   { text: "首页", link: "/", icon: 'T-shouye2' },
   { text: "分类", link: "/categories/qianduan/1/", icon: 'T-fenlei' },
-  { text: "标签", link: "/tags/css/1/", icon: 'T-24gl-tags3' },
+  { text: "标签", link: "/tags/fenxiang/1/", icon: 'T-24gl-tags3' },
 ]
