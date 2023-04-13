20015a0d17c8f97345eaa1c12951900dcf1fbac9
Toby <1289691918@qq.com>
2023-04-13 15:26:06 +0800

    PWA

--- .vuepress/head/index.js
+++ .vuepress/head/index.js
@@ -2,4 +2,5 @@ export default [
   ['link', { rel: 'icon', href: 'https://toby607-1317049696.cos.ap-guangzhou.myqcloud.com/images/202303201032565.ico' }],
   ['script', { type: 'module', async: true, src: '//at.alicdn.com/t/c/font_3933848_yndlegrc3op.js' }],
   ['meta', { name: 'referrer',content: 'no-referrer' }],
+  ['link', { rel: 'manifest', href: '../public/manifest.webmanifest' }],
 ]
