1558000ee7fca8c94a090f566931212919f58db5
Toby <1289691918@qq.com>
2023-04-25 10:55:40 +0800

    打包

--- .vuepress/dist/assets/index.html-23a99e15.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-3accc344","path":"/categories/CSS/1/","title":"","lang":"en-US","frontmatter":{"layout":"Categories"},"headers":[],"git":{},"filePathRelative":null}');export{e as data};
--- .vuepress/head/index.js
+++ .vuepress/head/index.js
@@ -2,7 +2,6 @@ export default [
   ['link', { rel: 'icon', href: 'https://toby607-1317049696.cos.ap-guangzhou.myqcloud.com/images/202303201032565.ico' }],
   ['script', { type: 'module', async: true, src: '//at.alicdn.com/t/c/font_3933848_yndlegrc3op.js' }],
   ['meta', { name: 'referrer',content: 'no-referrer' }],
-  ['link', { rel: 'manifest', href: '../public/manifest.webmanifest' }],
   ['meta', { name: 'keywords',content: 'Toby的学习基地,Toby的博客,Toby,toby,博客' }],
   ['meta', { name: 'description',content: '这是Toby的个人博客,个人空间随便写写，日常积累。' }],
 ]
